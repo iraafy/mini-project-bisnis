ab3431aadedbed7b4548f9e7de9f004b243e2114
Zahra Elgysha <79628357+zahraeln17@users.noreply.github.com>
2022-10-17 18:42:58 +0700

    edit opini

--- components/Opini.js
+++ components/Opini.js
@@ -27,38 +27,67 @@ export default function Opini(){
                     </div>
                 </div>
             </div>
+            
             <div className="container">
                 <div className="flex flex-wrap">
                     <div className="w-1/2 border-r-2 border-dotted border-blue-900 p-3">
                         <div className="border-b-2 border-blue-700">
-                            <h1 className="font-bold text-md">
-                                OPINI: Menjaga Denyut Ekonomi Stabil
-                            </h1>
-                            <p className="text-xs pt-4 pb-4">
+                            <div className="m-0 px-0 pt-0 py-2 grid sm:grid-flow-col-dense">
+                                <div className="container">
+                                    <h1 className="font-bold text-xs">
+                                        OPINI: Menjaga Denyut Ekonomi Stabil
+                                    </h1>
+                                </div>
+                                <div className="container">
+                                    <Image width={'50'} height={'50'} className="w-small pl-2" src="/images/j-pict-mini-2.jpeg" alt="Foto mini"/>
+                                </div>
+                            </div>
+                            <p className="text-sm pt-4 pb-4">
                                 28 Juli 2022 | Lim Hedaksi
                             </p>
                         </div>
-                        <h1 className="font-bold text-md pt-4">
-                            SPEKTRUM: Mengembalikan CFW Keasalnya
-                        </h1>
-                        <p className="text-xs pt-5 px-0">
-                            27 Juli 2022 | Lim Hedaksi
-                        </p>
-                    </div>
+                        <div className="m-0 px-0 pt-4 py-2 grid sm:grid-flow-col-dense">
+                                <div className="container">
+                                    <h1 className="font-bold text-xs">
+                                        SPEKTRUM: Mengembalikan CFW Keasalnya
+                                    </h1>
+                                </div>
+                                <div className="container">
+                                    <img className="w-full pl-2" src="/images/j-pict-mini-4.jpeg" alt="Foto mini"/>
+                                </div>
+                            </div>
+                            <p className="text-sm pt-5 pb-4">
+                                27 Juli 2022 | Dika Irawan
+                            </p>
+                        </div>
                     <div className="w-1/2 p-3">
                         <div className="border-b-2 border-blue-700">
-                            <h1 className="font-bold text-md">
-                                OPINI: Menjaga Denyut Ekonomi Stabil
-                            </h1>
-                            <p className="text-xs pt-4 pb-4">
-                                28 Juli 2022 | Lim Hedaksi
+                            <div className="m-0 px-0 pt-0 py-2 grid sm:grid-flow-col-dense">
+                                <div className="container">
+                                    <h1 className="font-bold text-xs">
+                                        EDITORIAL: Melirik Kembali Investasi Negeri Samurai
+                                    </h1>
+                                </div>
+                                <div className="container">
+                                    <img className="w-full pl-2" src="/images/j-pict-mini-4.jpeg" alt="Foto mini"/>
+                                </div>
+                            </div>
+                            <p className="text-sm pt-2 pb-4">
+                                27 Juli 2022 | Lim Hedaksi
                             </p>
                         </div>
-                        <h1 className="font-bold text-md pt-4">
-                            SPEKTRUM: Mengembalikan CFW Keasalnya
-                        </h1>
-                        <p className="text-xs pt-5 px-0">
-                            27 Juli 2022 | Lim Hedaksi
+                        <div className="m-0 px-0 pt-4 py-2 grid sm:grid-flow-col-dense">
+                            <div className="container">
+                                <h1 className="font-bold text-xs">
+                                    OPINI: Sinargi Menjamin Laju Kredit
+                                </h1>
+                            </div>
+                            <div className="container">
+                                <img className="w-full pl-2" src="/images/j-pict-mini-5.jpeg" alt="Foto mini"/>
+                            </div>
+                        </div>
+                        <p className="text-sm pt-5 pb-4">
+                            27 Juli 2022 | Jusuf Irianto
                         </p>
                     </div>
                 </div>
